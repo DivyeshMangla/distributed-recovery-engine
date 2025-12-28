7702c590b4c42253fc5f1990f69716c2583e4826
Divyesh Mangla <174847645+DivyeshMangla@users.noreply.github.com>
2025-12-28 15:16:53 +0530

    fix: handle lagging

--- internal/node/failure_monitor.go
+++ internal/node/failure_monitor.go
@@ -6,8 +6,17 @@ func (n *Node) startFailureMonitor() {
 	ticker := time.NewTicker(2 * time.Second)
 	defer ticker.Stop()
 
-	for range ticker.C {
+	last := time.Now()
+	interval := 2 * time.Second
+
+	for now := range ticker.C {
+		if now.Sub(last) > 2*interval {
+			last = now // lagged
+			continue
+		}
+
 		n.Membership.MarkSuspect(10 * time.Second)
 		n.Membership.MarkDead(30 * time.Second)
+		last = now
 	}
 }
